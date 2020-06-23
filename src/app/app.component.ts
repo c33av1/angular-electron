0486831bc15daa484992587297c030ab932e6c1f
c33av1 <chhavi.negi4@gmail.com>
2020-06-22 19:02:41 -0700

    Added timer for desktop app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { interval } from "rxjs";
+
+import { tap, take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-electron';
+  max = 1;
+  current = 0;
+
+  start() {
+    this.current = 0;
+    const maxTime = this.max;
+    // const _isFinished = !this.isFinished;
+
+    //emit value in sequence every 1 second
+    const source = interval(1000);
+
+    source
+      //.pipe(takeWhile((_) => _isFinished))
+      .pipe(take(maxTime))
+      .pipe(tap((i) => (this.current += 1)))
+      .subscribe();
+  }
+
+  /// reset timer
+  reset() {
+    this.current = 0;
+    this.max = 0;
+  }
+
+  // // getters to prevent NaN errors
+  get maxVal() {
+    return isNaN(this.max) || this.max < 1 ? 1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return parseFloat(`${this.currentVal}`) >= parseFloat(`${this.maxVal}`);
+  }
 }
